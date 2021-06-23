cfc8034372caed8441c1babe3ffa1e7ab2ef20be
abdusalom020 <acridsss@gmail.com>
2021-06-23 23:40:07 +0500

    faq

---
+++ js/main.js
@@ -0,0 +1,41 @@
+var modal = document.getElementById("modals");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("modal-btn__close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// var elsModal = document.querySelectorAll('#modals');
+// var elsBtn = document.querySelectorAll('#myBtn');
+// var elsClose = document.querySelectorAll('.modal-btn__close');
+
+// elsBtn.addEventListener('click', function () {
+//   elsModal.classList.add('modal__open');
+// });
+
+// elsClose.addEventListener('click', function () {
+//   elsModal.classList.remove('modal__open');
+// });
+
+var elsAnsverBtn = document.querySelectorAll('.ansver-top__btn');
+var elsAnsversItem = document.querySelectorAll('.ansvers__item');
+
+
+elsAnsverBtn.forEach(function (toggle) {
+  toggle.addEventListener('click', function (){
+    toggle.closest('.ansvers__item').classList.toggle('ansvers--open')
+  })
+})
